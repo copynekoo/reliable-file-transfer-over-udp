6c8a443aab29717b63e3e1b59dbfac27bac146d7
copynekoo <srisil.s@ku.th>
2025-10-07 22:44:19 +0700

    Simple file transfer using UDP

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import argparse
+import socket
+from pathlib import Path
 
 # Settings
 parser = argparse.ArgumentParser(
@@ -21,3 +23,36 @@ Port: %s
 Filename: %s'''
   % (server_ip, port, filename)
 )
+
+# Directory where downloaded files are stored
+client_folder = './client/'
+Path(client_folder).mkdir(parents=True, exist_ok=True) # Create client folder if the folder does not exist
+
+# Request to server
+port = int(port)
+address = (server_ip, port)
+fileName = bytes(filename, 'utf-8')
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Request to server to check if file exists
+sock.sendto(fileName, address)
+decoded_fileName = fileName.decode('utf-8')
+print("Requesting file:", decoded_fileName)
+data, addr = sock.recvfrom(1024)
+exist = int(data)
+
+if exist:
+    print("File is exist and initiating download.")
+    fileName = fileName.decode('utf-8')
+    downloaded_file = open(client_folder + fileName, "wb")
+    while True:
+        data, addr = sock.recvfrom(1024)
+        if data:
+            downloaded_file.write(data)
+        if not data:
+            downloaded_file.close()
+            break
+
+    print("File is downloaded.")
+else:
+    print("File does not exist.")
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import argparse
+import socket
 
 # Settings
 parser = argparse.ArgumentParser(
@@ -11,3 +12,35 @@ args = parser.parse_args()
 port = args.port
 
 print("Port:", port)
+
+server_folder = './server/'
+buffer_size = 1024
+
+# Response to client
+server_ip = '127.0.0.1'
+port = int(port)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((server_ip, port))
+
+print("Server is running and waiting for request.")
+while True:
+  data, addr = sock.recvfrom(buffer_size)
+  fileName = data.decode('utf-8')
+  print("Requested file:", fileName)
+
+  try:
+    a = server_folder + fileName
+
+    with open(a, 'rb') as file:
+      print("File exists and is ready to read")
+      sock.sendto(b"1", addr)
+      data = file.read(buffer_size)
+      sock.sendto(data, addr)
+      while data:
+        data = file.read(buffer_size)
+        sock.sendto(data, addr)
+
+      print(fileName, "reached EoF")
+  except FileNotFoundError:
+    print("File does not exist")
+    sock.sendto(b"0", addr)
